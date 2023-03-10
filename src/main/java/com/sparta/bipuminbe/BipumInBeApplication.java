06562e7bc222a27f32dc06b2fa2956622dc16ef3
bestfarmer94 <sjasjaruddus@naver.com>
2023-03-10 23:00:17 +0900

    feat. 초기세팅.

---
+++ src/main/java/com/sparta/bipuminbe/BipumInBeApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.bipuminbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BipumInBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BipumInBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/bipuminbe/BipumInBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.bipuminbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BipumInBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
