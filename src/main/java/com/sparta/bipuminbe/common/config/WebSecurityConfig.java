d3448b9814bb74b02b8c4b93e8a364691f6def4a
DOGWANNA <9401ndk@naver.com>
2023-03-14 22:42:42 +0900

    feat: Https 오류 수정

--- src/main/java/com/sparta/bipuminbe/common/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/bipuminbe/common/config/WebSecurityConfig.java
@@ -92,7 +92,8 @@ public class WebSecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         // 사전에 약속된 출처를 명시
-        config.addAllowedOrigin("**");
+//        config.addAllowedOrigin("*");
+        config.addAllowedOrigin("http://localhost:3000");
         config.addAllowedOrigin("http://localhost:3001");
         config.addAllowedOrigin("http://hanghae1teamwork.s3-website.ap-northeast-2.amazonaws.com/");
 //        config.addAllowedOrigin("http://rolling-rolling.s3-website.ap-northeast-2.amazonaws.com/");
