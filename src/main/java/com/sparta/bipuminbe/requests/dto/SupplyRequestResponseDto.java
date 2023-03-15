8840f0c88c9f7fa34fd8c9883d0365673e24d6a0
bestfarmer94 <sjasjaruddus@naver.com>
2023-03-15 20:56:19 +0900

    feat: Admin Requests Supply Details 페이지 구현

---
+++ src/main/java/com/sparta/bipuminbe/requests/dto/SupplyRequestResponseDto.java
@@ -0,0 +1,45 @@
+package com.sparta.bipuminbe.requests.dto;
+
+import com.sparta.bipuminbe.common.entity.Category;
+import com.sparta.bipuminbe.common.entity.Requests;
+import com.sparta.bipuminbe.common.entity.User;
+import com.sparta.bipuminbe.common.enums.UserRoleEnum;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class SupplyRequestResponseDto {
+    private String requestType;
+    private Long requestId;
+    private Boolean isAdmin;
+    private Long userId;
+    private String deptName;
+    private String empName;
+    private Long categoryId;
+    private String categoryName;
+    private String content;
+    private String requestStatus;
+    private LocalDateTime createdAt;
+
+    public static SupplyRequestResponseDto of(Requests requests) {
+        User user = requests.getUser();
+        Category category = requests.getCategory();
+
+        return SupplyRequestResponseDto.builder()
+                .requestType(requests.getRequestType().getKorean())
+                .requestId(requests.getRequestId())
+                .isAdmin(user.getRole().equals(UserRoleEnum.ADMIN))
+                .userId(user.getId())
+                .deptName(user.getDepartment().getDeptName())
+                .empName(user.getEmpName())
+                .categoryId(category.getId())
+                .categoryName(category.getCategoryName())
+                .content(requests.getContent())
+                .requestStatus(requests.getRequestStatus().getKorean())
+                .createdAt(requests.getCreatedAt())
+                .build();
+    }
+}
