38401b362b4137db0472a802049d12ea13cfa0d5
DOGWANNA <9401ndk@naver.com>
2023-03-16 02:20:55 +0900

    feat: 추가 정보 기입 페이지 추가

--- src/main/java/com/sparta/bipuminbe/user/controller/UserController.java
+++ src/main/java/com/sparta/bipuminbe/user/controller/UserController.java
@@ -2,10 +2,12 @@ package com.sparta.bipuminbe.user.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.sparta.bipuminbe.common.dto.ResponseDto;
+import com.sparta.bipuminbe.common.security.UserDetailsImpl;
 import com.sparta.bipuminbe.user.dto.LoginRequestDto;
 import com.sparta.bipuminbe.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -20,13 +22,13 @@ public class UserController {
     @GetMapping("/kakao/callback")
     public ResponseEntity<ResponseDto<Boolean>> kakaoLogin(@RequestParam String code) throws JsonProcessingException {
         // code: 카카오 서버로부터 받은 인가 코드
-
         return kakaoService.kakaoLogin(code);
     }
 
+    //로그인 시, 부서와 유저이름이 없는 경우 반드시 추가입력하게 유도
     @PostMapping("/loginadd")
-    public String loginAdd(@Valid @RequestBody LoginRequestDto loginRequestDto){
-//        kakaoService.loginAdd(loginRequestDto);
-        return "1";
+    public ResponseDto<String> loginAdd(@Valid @RequestBody LoginRequestDto loginRequestDto, @AuthenticationPrincipal UserDetailsImpl userDetails){
+        // 카카오에서 받아오지 못하는 유저이름과 부서 추가입력
+        return  kakaoService.loginAdd(loginRequestDto, userDetails.getUser());
     }
 }
