65335d8b5730e73d347ceea29867f2ed26a4c649
bestfarmer94 <sjasjaruddus@naver.com>
2023-03-15 23:49:01 +0900

    feat: Admin Requests Repair Details 중간 저장

---
+++ src/main/java/com/sparta/bipuminbe/requests/dto/RepairRequestResponseDto.java
@@ -0,0 +1,48 @@
+package com.sparta.bipuminbe.requests.dto;
+
+import com.sparta.bipuminbe.common.entity.Requests;
+import com.sparta.bipuminbe.common.entity.Supply;
+import com.sparta.bipuminbe.common.entity.User;
+import com.sparta.bipuminbe.common.enums.UserRoleEnum;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class RepairRequestResponseDto {
+    private String requestType;
+    private Long requestId;
+    private Boolean isAdmin;
+    private Long userId;
+    private String deptName;
+    private String empName;
+    private Long supplyId;
+    private String modelName;
+    private String image;
+    private String content;
+    private String requestStatus;
+    private LocalDateTime createdAt;
+    private Boolean isSelf;
+    private Boolean isRead;
+
+    public static RepairRequestResponseDto of(Requests requests) {
+        User user = requests.getUser();
+        Supply supply = requests.getSupply();
+
+        return RepairRequestResponseDto.builder()
+                .requestType(requests.getRequestType().getKorean())
+                .requestId(requests.getRequestId())
+                .isAdmin(user.getRole().equals(UserRoleEnum.ADMIN))
+                .userId(user.getId())
+                .deptName(user.getDepartment().getDeptName())
+                .empName(user.getEmpName())
+                .supplyId(supply.getSupplyId())
+                .modelName(supply.getModelName())
+                .content(requests.getContent())
+                .requestStatus(requests.getRequestStatus().getKorean())
+                .createdAt(requests.getCreatedAt())
+                .build();
+    }
+}
--- src/main/java/com/sparta/bipuminbe/requests/service/RepairRequestService.java
+++ src/main/java/com/sparta/bipuminbe/requests/service/RepairRequestService.java
@@ -1,9 +1,18 @@
 package com.sparta.bipuminbe.requests.service;
 
+import com.sparta.bipuminbe.common.dto.ResponseDto;
+import com.sparta.bipuminbe.common.entity.User;
+import com.sparta.bipuminbe.requests.dto.RepairRequestResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class RepairRequestService {
+    private final RequestsService requestsService;
+
+
+//    public ResponseDto<RepairRequestResponseDto> getRepairRequest(Long requestId, User user) {
+//
+//    }
 }
