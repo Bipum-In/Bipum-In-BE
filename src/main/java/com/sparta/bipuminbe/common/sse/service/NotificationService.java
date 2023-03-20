d9183d3264428afc38e7fde1cc73c4bc9103e025
DOGWANNA <9401ndk@naver.com>
2023-03-21 02:28:16 +0900

    feat: SSE 관련 로그 확인6

--- src/main/java/com/sparta/bipuminbe/common/sse/service/NotificationService.java
+++ src/main/java/com/sparta/bipuminbe/common/sse/service/NotificationService.java
@@ -34,23 +34,24 @@ public class NotificationService {
     public SseEmitter subscribe(Long userId, String lastEventId) {
         String emitterId = makeTimeIncludeId(userId);
         // lastEventId가 있을 경우, userId와 비교해서 유실된 데이터일 경우 재전송할 수 있다.
-        log.info("subscribe1");
+        log.info("save 전");
         SseEmitter emitter = emitterRepository.save(emitterId, new SseEmitter(DEFAULT_TIMEOUT));
-        log.info("subscribe2");
+        log.info("save 후");
+
+        log.info("userId : " + userId);
+        log.info("emitterId " + emitterId);
+        log.info("lastEventId : " + lastEventId);
+
         //시간이 만료된 경우 자동으로 레포지토리에서 삭제하고 클라이언트에서 재요청을 보낸다.
         emitter.onCompletion(() -> emitterRepository.deleteById(emitterId));
-        log.info("subscribe3");
+        log.info("onCompletion 후");
         emitter.onTimeout(() -> emitterRepository.deleteById(emitterId));
-        log.info("subscribe4");
+        log.info("Timeout 후");
         //Dummy 데이터를 보내 503에러 방지. (SseEmitter 유효시간 동안 어느 데이터도 전송되지 않으면 503에러 발생)
         String eventId = makeTimeIncludeId(userId);
         log.info("subscribe5");
         sendNotification(emitter, eventId, emitterId, "EventStream Created. [userId=" + userId + "]");
         log.info("subscribe6");
-
-        log.info("userId : " + userId);
-        log.info("emitterId " + emitterId);
-        log.info("lastEventId : " + lastEventId);
 
         // 클라이언트가 미수신한 Event 목록이 존재할 경우 전송하여 Event 유실을 예방한다.
         if (hasLostData(lastEventId)) {
@@ -58,7 +59,6 @@ public class NotificationService {
             sendLostData(lastEventId, userId, emitterId, emitter);
             log.info("subscribe8");
         }
-        log.info("lastEventId : " + lastEventId);
         return emitter;
     }
 
@@ -68,10 +68,13 @@ public class NotificationService {
 
     private void sendNotification(SseEmitter emitter, String eventId, String emitterId, Object data) {
         try {
+            log.info("eventId : " + eventId);
+            log.info("data" + data);
             emitter.send(SseEmitter.event()
                     .id(eventId)
                     .data(data));
         } catch (IOException exception) {
+            log.info("예외 발생해서 emitter 삭제됨");
             emitterRepository.deleteById(emitterId);
         }
     }
