734355c525b1cdca521f45f039f448834909f583
bestfarmer94 <sjasjaruddus@naver.com>
2023-03-27 16:44:12 +0900

    feat: Soft Delete Entity 적용.

--- src/main/java/com/sparta/bipuminbe/common/entity/Supply.java
+++ src/main/java/com/sparta/bipuminbe/common/entity/Supply.java
@@ -6,6 +6,8 @@ import com.sparta.bipuminbe.common.exception.ErrorCode;
 import com.sparta.bipuminbe.supply.dto.SupplyRequestDto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -16,6 +18,8 @@ import static com.sparta.bipuminbe.common.enums.SupplyStatusEnum.USING;
 @Entity
 @Getter
 @NoArgsConstructor
+@SQLDelete(sql = "UPDATE Supply SET deleted = true WHERE supplyId = ?")
+@Where(clause = "deleted = false")  // 조회할 때 false만 찾는 것이 default 가 된다.
 public class Supply extends TimeStamped {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -45,6 +49,8 @@ public class Supply extends TimeStamped {
     @JoinColumn(name = "categoryId", nullable = false)
     private Category category;
 
+    @Column(nullable = false)
+    private Boolean deleted;
 
     public Supply(SupplyRequestDto supplyRequestDto, Partners partners, Category category, User user, String image) {
         this.serialNum = supplyRequestDto.getSerialNum();
