dffa7994f58ee7e54f50ee7c8d35c8e46e02be8d
DOGWANNA <9401ndk@naver.com>
2023-03-28 16:15:08 +0900

    feat: 알림 필드에 작성시간 추가

--- src/main/java/com/sparta/bipuminbe/common/sse/dto/NotificationResponseDto.java
+++ src/main/java/com/sparta/bipuminbe/common/sse/dto/NotificationResponseDto.java
@@ -3,18 +3,23 @@ package com.sparta.bipuminbe.common.sse.dto;
 import com.sparta.bipuminbe.common.sse.entity.Notification;
 import lombok.Builder;
 import lombok.Getter;
+import org.joda.time.DateTime;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Builder
 public class NotificationResponseDto {
     private String content;
     private String url;
+    private LocalDateTime createdAt;
 
-    public static NotificationResponseDto of(Notification notification){
+    public static NotificationResponseDto of(Notification notification) {
 
-        return builder().
-                content(notification.getContent()).
-                url(notification.getUrl())
+        return builder()
+                .content(notification.getContent())
+                .url(notification.getUrl())
+                .createdAt(notification.getCreatedAt())
                 .build();
     }
 }
