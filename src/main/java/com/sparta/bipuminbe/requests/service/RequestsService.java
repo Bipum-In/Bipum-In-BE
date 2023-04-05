f215047a9ae4ec530d0c9d0e870e4c01be286d72
bestfarmer94 <sjasjaruddus@naver.com>
2023-04-06 03:26:03 +0900

    feat: 비품 요청의 경우 개인/공용 Valid check 메서드 추가.

--- src/main/java/com/sparta/bipuminbe/requests/service/RequestsService.java
+++ src/main/java/com/sparta/bipuminbe/requests/service/RequestsService.java
@@ -61,7 +61,9 @@ public class RequestsService {
 //        );
         if (requestsRequestDto.getRequestType().name().equals("SUPPLY")) {
             Category category = getCategory(requestsRequestDto.getCategoryId());
-
+            if(requestsRequestDto.getUseType() == null) {
+                throw new CustomException(ErrorCode.InValidRequest);
+            }
             Requests createRequests = requestsRepository.save(Requests.builder()
                     .content(requestsRequestDto.getContent())
                     .requestType(requestsRequestDto.getRequestType())
