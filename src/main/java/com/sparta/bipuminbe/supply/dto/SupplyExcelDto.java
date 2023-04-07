f3960f7d20331e3bc4fddfba6be1004c211ba933
bestfarmer94 <sjasjaruddus@naver.com>
2023-04-07 19:05:16 +0900

    fix: 요청 페이지. 공용 비품 목록 요청가능한 목록만 뜨도록 로직 수정.

--- src/main/java/com/sparta/bipuminbe/supply/dto/SupplyExcelDto.java
+++ src/main/java/com/sparta/bipuminbe/supply/dto/SupplyExcelDto.java
@@ -3,7 +3,6 @@ package com.sparta.bipuminbe.supply.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
