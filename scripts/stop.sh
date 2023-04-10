b641438c621ec4e053161d0e5c958913243d5be8
bestfarmer94 <sjasjaruddus@naver.com>
2023-04-10 21:58:17 +0900

    fix: CI/CD 오류 fix3

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 PROJECT_ROOT="/home/ubuntu/app"
-JAR_FILE="$PROJECT_ROOT/Bipum-In-BE-0.0.1-SNAPSHOT.jar"
+JAR_FILE="$PROJECT_ROOT/spring-webapp.jar"
 
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
