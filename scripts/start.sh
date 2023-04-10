c5589698c8c2c072fd6c1ac0c3d56fb70365397d
bestfarmer94 <sjasjaruddus@naver.com>
2023-04-10 17:50:47 +0900

    chore : CICD (자동배포시 이미지 업로드 Permission)

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 PROJECT_ROOT="/home/ubuntu/app"
-JAR_FILE="$PROJECT_ROOT/Bipum-In-BE-0.0.1-SNAPSHOT.jar"
+JAR_FILE="$PROJECT_ROOT/spring-webapp.jar"
 
 APP_LOG="$PROJECT_ROOT/application.log"
 ERROR_LOG="$PROJECT_ROOT/error.log"
