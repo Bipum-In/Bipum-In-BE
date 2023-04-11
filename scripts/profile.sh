98633c278a1184692c171c3e687e04e97e78c73a
DOGWANNA <9401ndk@naver.com>
2023-04-11 09:17:01 +0900

    feat: Docker 파일 및 무중단 배포 설정파일 추가

---
+++ scripts/profile.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+function find_idle_profile()
+{
+    RESPONSE_CODE=$(sudo curl -s -o /dev/null -w "%{http_code}" http://15.164.166.126/)
+
+    if [ ${RESPONSE_CODE} -ge 400 ] # 400 보다 크면 (즉, 40x/50x 에러 모두 포함)
+    then
+        CURRENT_PROFILE=real2
+    else
+        CURRENT_PROFILE=$(sudo curl -s http://15.164.166.126/)
+    fi
+
+    if [ ${CURRENT_PROFILE} == real1 ]
+    then
+      IDLE_PROFILE=real2
+    else
+      IDLE_PROFILE=real1
+    fi
+
+    echo "${IDLE_PROFILE}"
+}
+# 쉬고 있는 profile의 port 찾기
+function find_idle_port()
+{
+    IDLE_PROFILE=$(find_idle_profile)
+
+    if [ ${IDLE_PROFILE} == real1 ]
+    then
+      echo "8081"
+    else
+      echo "8082"
+    fi
+}
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,19 +1,42 @@
-#!/usr/bin/env bash
+##!/usr/bin/env bash
+#
+#PROJECT_ROOT="/home/ubuntu/app"
+#JAR_FILE="$PROJECT_ROOT/spring-webapp.jar"
+#
+#DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
+#
+#TIME_NOW=$(date +%c)
+#
+## 현재 구동 중인 애플리케이션 pid 확인
+#CURRENT_PID=$(pgrep -f $JAR_FILE)
+#
+## 프로세스가 켜져 있으면 종료
+#if [ -z $CURRENT_PID ]; then
+#  echo "$TIME_NOW > 현재 실행중인 애플리케이션이 없습니다" >> $DEPLOY_LOG
+#else
+#  echo "$TIME_NOW > 실행중인 $CURRENT_PID 애플리케이션 종료 " >> $DEPLOY_LOG
+#  kill -15 $CURRENT_PID
+#fi
 
-PROJECT_ROOT="/home/ubuntu/app"
-JAR_FILE="$PROJECT_ROOT/spring-webapp.jar"
 
-DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
+ABSPATH=$(readlink -f $0)
+ABSDIR=$(dirname $ABSPATH)
+source ${ABSDIR}/profile.sh
 
-TIME_NOW=$(date +%c)
+IDLE_PROFILE=$(find_idle_profile)
 
-# 현재 구동 중인 애플리케이션 pid 확인
-CURRENT_PID=$(pgrep -f $JAR_FILE)
+CONTAINER_ID=$(docker container ls -f "name=${IDLE_PROFILE}" -q)
 
-# 프로세스가 켜져 있으면 종료
-if [ -z $CURRENT_PID ]; then
-  echo "$TIME_NOW > 현재 실행중인 애플리케이션이 없습니다" >> $DEPLOY_LOG
+echo "> 컨테이너 ID는 무엇?? ${CONTAINER_ID}"
+echo "> 현재 프로필은 무엇?? ${IDLE_PROFILE}"
+
+if [ -z ${CONTAINER_ID} ]
+then
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-  echo "$TIME_NOW > 실행중인 $CURRENT_PID 애플리케이션 종료 " >> $DEPLOY_LOG
-  kill -15 $CURRENT_PID
+  echo "> docker stop ${IDLE_PROFILE}"
+  sudo docker stop ${IDLE_PROFILE}
+  echo "> docker rm ${IDLE_PROFILE}"
+  sudo docker rm ${IDLE_PROFILE}    # 컨테이너 이름을 지정해서 사용하기 때문에.. 꼭 컨테이너 삭제도 같이 해주셔야 합니다. (나중에 다시 띄울거기 때문에..)
+  sleep 5
 fi
