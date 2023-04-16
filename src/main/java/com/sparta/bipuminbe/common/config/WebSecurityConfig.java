f370a5bf384275f7d044defff2cc0ec2945ed7da
bestfarmer94 <sjasjaruddus@naver.com>
2023-04-16 15:15:12 +0900

    fix: front 서버 https 도메인 security 및 properties, secretkey 오류로 인한 변경

--- src/main/java/com/sparta/bipuminbe/common/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/bipuminbe/common/config/WebSecurityConfig.java
@@ -79,7 +79,7 @@ public class WebSecurityConfig {
         config.addAllowedOrigin("http://hanghae77.s3-website.ap-northeast-2.amazonaws.com");
         config.addAllowedOrigin("http://hanghae1teamwork.s3-website.ap-northeast-2.amazonaws.com/");
         config.addAllowedOrigin("https://bipum-in-fe-two.vercel.app/");
-        config.addAllowedOrigin("https://bipumin.shop/");
+        config.addAllowedOrigin("https://www.bipumin.shop");
 
         // 특정 헤더를 클라이언트 측에서 사용할 수 있게 지정
         // 만약 지정하지 않는다면, Authorization 헤더 내의 토큰 값을 사용할 수 없음
