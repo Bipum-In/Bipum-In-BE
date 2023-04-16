b6488d0ad0ae2a6179e4165f5000ec269f9d4ae4
bestfarmer94 <sjasjaruddus@naver.com>
2023-04-16 21:17:55 +0900

    fix: UserRoleEnum Authority 수정.

--- src/main/java/com/sparta/bipuminbe/common/enums/UserRoleEnum.java
+++ src/main/java/com/sparta/bipuminbe/common/enums/UserRoleEnum.java
@@ -1,15 +1,17 @@
 package com.sparta.bipuminbe.common.enums;
 
 public enum UserRoleEnum {
-    USER(Authority.USER), // 사용자 권한
-    ADMIN(Authority.ADMIN), // 관리자 권한
-    MASTER(Authority.MASTER), // 마스터 권한
-    RESPONSIBILITY(Authority.RESPONSIBILITY);   // 책임자 권한
+    USER(Authority.USER, "유저"), // 사용자 권한
+    ADMIN(Authority.ADMIN, "비품 총괄 관리자"), // 관리자 권한
+    MASTER(Authority.MASTER, "마스터"), // 마스터 권한
+    RESPONSIBILITY(Authority.RESPONSIBILITY, "공용 비품 책임자");   // 책임자 권한
 
     private final String authority;
+    private final String korean;
 
-    UserRoleEnum(String authority) {
+    UserRoleEnum(String authority, String korean) {
         this.authority = authority;
+        this.korean = korean;
     }
 
     public String getAuthority() {
@@ -17,9 +19,9 @@ public enum UserRoleEnum {
     }
 
     public static class Authority {
-        public static final String USER = "ROLE_USER";
-        public static final String ADMIN = "ROLE_ADMIN";
-        public static final String MASTER = "ROLE_MASTER";
-        public static final String RESPONSIBILITY = "ROLE_RESPONSIBILITY";
+        public static final String USER = "ROLE_USER";  // 유저
+        public static final String ADMIN = "ROLE_ADMIN";    // 비품 총괄 관리자
+        public static final String MASTER = "ROLE_MASTER";  // 마스터
+        public static final String RESPONSIBILITY = "ROLE_RESPONSIBILITY";  // 공용 비품 책임자
     }
 }
