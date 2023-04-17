0b2c74e137317941993d78ce81532124fa0f82e8
DOGWANNA <9401ndk@naver.com>
2023-04-18 01:00:57 +0900

    style: 로그 추가

--- src/main/java/com/sparta/bipuminbe/common/jwt/JwtAuthFilter.java
+++ src/main/java/com/sparta/bipuminbe/common/jwt/JwtAuthFilter.java
@@ -33,14 +33,16 @@ public class JwtAuthFilter extends OncePerRequestFilter {
         String accessToken = null;
         String refreshToken = null;
 
-        if(cookies != null){
-            for(Cookie cookie : cookies){
-                if(cookie != null){
-                    if(cookie.getName().equals(JwtUtil.AUTHORIZATION_HEADER)){
-                        accessToken = jwtUtil.resolveToken(cookie);
-                    }else if(cookie.getName().equals(JwtUtil.REFRESH_HEADER)){
-                        refreshToken = jwtUtil.resolveToken(cookie);
-                    }
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie == null) {
+                    continue;
+                }
+                log.info(cookie.getName(), cookie.getValue());
+                if (cookie.getName().equals(JwtUtil.AUTHORIZATION_HEADER)) {
+                    accessToken = jwtUtil.resolveToken(cookie);
+                } else if (cookie.getName().equals(JwtUtil.REFRESH_HEADER)) {
+                    refreshToken = jwtUtil.resolveToken(cookie);
                 }
             }
         }
