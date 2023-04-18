728297f207c94617342b592b3af33a6ac8f14a0a
DOGWANNA <9401ndk@naver.com>
2023-04-19 00:32:36 +0900

    feat: Notification 관련 JPQL 쿼리 DSL로 변경

--- src/main/java/com/sparta/bipuminbe/common/queryDSL/notification/NotificationRepositoryCustom.java
+++ src/main/java/com/sparta/bipuminbe/common/queryDSL/notification/NotificationRepositoryCustom.java
@@ -1,12 +1,15 @@
 package com.sparta.bipuminbe.common.queryDSL.notification;
 
+import com.sparta.bipuminbe.common.entity.Notification;
 import com.sparta.bipuminbe.common.sse.dto.NotificationResponseForAdmin;
 import com.sparta.bipuminbe.common.sse.dto.NotificationResponseForUser;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface NotificationRepositoryCustom {
     Page<NotificationResponseForAdmin> findUserNotification(Long adminId, Pageable pageable);
     Page<NotificationResponseForUser> findAdminNotification(Long userId, Pageable pageable);
+    List<Notification> findOldNotification();
 }
--- src/main/java/com/sparta/bipuminbe/common/queryDSL/notification/NotificationRepositoryImpl.java
+++ src/main/java/com/sparta/bipuminbe/common/queryDSL/notification/NotificationRepositoryImpl.java
@@ -1,8 +1,11 @@
 package com.sparta.bipuminbe.common.queryDSL.notification;
 
-import com.querydsl.core.Tuple;
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.sparta.bipuminbe.common.entity.Notification;
 import com.sparta.bipuminbe.common.entity.QNotification;
 import com.sparta.bipuminbe.common.entity.QRequests;
 import com.sparta.bipuminbe.common.entity.QUser;
@@ -13,36 +16,79 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
 public class NotificationRepositoryImpl implements NotificationRepositoryCustom{
     private final JPAQueryFactory jpaQueryFactory;
-    //
-    // 기존에 join한 데이터를 받아오는 방법을 어떻게 변경할 것인지가 관건
+
+    // notification의 request_id 와 같은 요청과 그 요청에 해당하는 관리자의 정보를 가져온다 (승인/폐기 등 처리 건만)
+    // 요청의 수신자가 userId에 해당하는 알림 건만 가져온다.
     public Page<NotificationResponseForAdmin> findUserNotification(Long adminId, Pageable pageable) {
-//        QNotification notification = QNotification.notification;
-//        QRequests request = QRequests.requests;
-//        QUser user = QUser.user;
-//
-//        JPAQuery<Tuple> query = jpaQueryFactory.select(notification.content, notification.createdAt, notification.acceptResult, request.requestId, notification.id, request.requestType)
-//                .from(notification)
-//                .join(notification.request, request)
-//                .join(request.user, user)
-//                .where(notification.receiver.id.eq(adminId)
-//                        .and(notification.notificationType.eq(NotificationType.REQUEST))
-//                        .and(notification.isRead.eq(false)))
-//                .orderBy(notification.createdAt.desc());
-//
-//        return PageableExecutionUtils.getPage(query.fetch(), pageable, )
-        return null;
+        QNotification notification = QNotification.notification;
+        QRequests request = QRequests.requests;
+        QUser user = QUser.user;
+
+        JPAQuery<NotificationResponseForAdmin> query = jpaQueryFactory.select(Projections.bean(NotificationResponseForAdmin.class,
+                        notification.content, user.image, notification.createdAt,
+                        request.requestId, notification.id, request.requestType))
+                .from(notification)
+                .join(request).on(notification.request.requestId.eq(request.requestId))
+                .join(user).on(request.user.id.eq(user.id))
+                .where(notification.receiver.id.eq(adminId)
+                        .and(notification.notificationType.eq(NotificationType.REQUEST))
+                        .and(notification.isRead.eq(false)))
+                .orderBy(notification.createdAt.desc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize());
+
+        List<NotificationResponseForAdmin> notificationList = query.fetch();
+        long totalCount = query.fetchCount();
+
+        return new PageImpl<>(notificationList, pageable, totalCount);
     }
 
+    // notification의 requests_id 와 같은 요청과 그 요청에 해당하는 유저의 정보를 가져온다 (요청 건만)
+    // 요청의 수신자가 adminId에 해당하는 알림 건만 가져온다.
     public Page<NotificationResponseForUser> findAdminNotification(Long userId, Pageable pageable) {
-        return null;
+        QNotification notification = QNotification.notification;
+        QRequests requests = QRequests.requests;
+        QUser user = QUser.user;
+
+        JPAQuery<NotificationResponseForUser> query = jpaQueryFactory.select(Projections.bean(
+                                NotificationResponseForUser.class,
+                                notification.content, notification.createdAt, notification.acceptResult,
+                                requests.requestId, notification.id, requests.requestType
+                        )
+                )
+                .from(notification)
+                .join(requests).on(notification.request.requestId.eq(requests.requestId))
+                .join(user).on(requests.admin.id.eq(user.id))
+                .where(notification.receiver.id.eq(userId)
+                        .and(notification.notificationType.eq(NotificationType.PROCESSED))
+                        .and(notification.isRead.eq(false)))
+                .orderBy(notification.createdAt.desc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize());
+
+        List<NotificationResponseForUser> notificationList = query.fetch();
+        long totalCount = query.fetchCount();
+
+        return new PageImpl<>(notificationList, pageable, totalCount);
+    }
+
+    public List<Notification> findOldNotification() {
+        QNotification notification = QNotification.notification;
+        BooleanExpression isRead = notification.isRead.eq(true);
+        BooleanExpression createdAt = notification.createdAt.before(LocalDateTime.now().minusDays(1));
+        BooleanExpression now = Expressions.asBoolean(true);
+
+        return jpaQueryFactory.selectFrom(notification)
+                .where(isRead, createdAt, now)
+                .fetch();
     }
 }
