bd73fb0757250527615a2b5c8c0dadc769776c6a
bestfarmer94 <sjasjaruddus@naver.com>
2023-04-21 11:30:37 +0900

    Refactoring: Requests Service 완료.

--- src/main/java/com/sparta/bipuminbe/requests/repository/RequestsRepository.java
+++ src/main/java/com/sparta/bipuminbe/requests/repository/RequestsRepository.java
@@ -22,10 +22,12 @@ public interface RequestsRepository extends JpaRepository<Requests, Long>, Reque
 
     boolean existsBySupply_SupplyIdAndRequestStatusNot(Long supplyId, RequestStatus requestStatus);
 
+    // 비품 History 조회. (승인 건만.)
     Page<Requests> findBySupply_SupplyIdAndRequestTypeInAndAcceptResult(Long supplyId, Set<RequestType> requestTypeQuery, AcceptResult accept, Pageable pageable);
 
     // 비품 폐기 전 요청들 처리. (수리중 포함.)
     List<Requests> findBySupply_SupplyIdAndRequestStatusNot(Long supplyId, RequestStatus requestStatus);
+
     // 유저 탈퇴 전 요청들 처리. (수리중 불포함.)
     List<Requests> findByUser_IdAndRequestStatus(Long id, RequestStatus unprocessed);
 }
